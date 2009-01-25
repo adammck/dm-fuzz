e3e4135d194e784dd030261c5532bf605bde2d6e
Adam Mckaig <adam.mckaig@gmail.com>
2009-01-25 12:27:13 -0500

    added storage tests for gender

--- spec/types/gender.rb
+++ spec/types/gender.rb
@@ -16,4 +16,20 @@ describe (klass = DataMapper::Fuzz::Gender) do
 			end
 		end
 	end
+	
+	describe "Storage" do
+		before(:each) do
+			@c = Child.new
+		end
+		
+		it "stores :male and :female as integers" do
+			klass.dump(:male).should == 0
+			klass.dump(:female).should == 1
+		end
+		
+		it "loads 0 and 1 as symbols" do
+			klass.load(0).should == :male
+			klass.load(1).should == :female
+		end
+	end
 end
